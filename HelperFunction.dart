22582038db8883a906916ea5316eb6136eed1089
Husen Ansari <HusenAnsari@users.noreply.github.com>
2021-09-12 17:47:32 +0530

    Add files via upload

---
+++ HelperFunction.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:html/parser.dart';
+
+class HelperFunction {
+  static Future<bool> isInternetConnected() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      return false;
+    }
+    return false;
+  }
+
+  static String parseHtmlString(String htmlString) {
+    final document = parse(htmlString);
+    final String parsedString = parse(document.body.text).documentElement.text;
+    return parsedString;
+  }
+}
